d8cccb104c306188f534b90e6cc5cd91d614fc92
myOmikron <git@omikron.dev>
2022-05-17 04:50:59 +0200

    Added checks for config

--- conf/config.go
+++ conf/config.go
@@ -1,5 +1,10 @@
 package conf
 
+import (
+	"errors"
+	"strings"
+)
+
 type Database struct {
 	Driver   string
 	Port     uint16
@@ -27,4 +32,36 @@ type Server struct {
 type Config struct {
 	Server   Server
 	Database Database
+}
+
+func (c *Config) Fix() error {
+	if c.Server.StaticDir == "" {
+		return errors.New("StaticDir must not be empty")
+	} else if !strings.HasSuffix(c.Server.StaticDir, "/") {
+		c.Server.StaticDir += "/"
+	}
+
+	if c.Server.TemplateDir == "" {
+		return errors.New("TemplateDir must not be empty")
+	} else if !strings.HasSuffix(c.Server.TemplateDir, "/") {
+		c.Server.TemplateDir += "/"
+	}
+
+	if c.Server.PGPDir == "" {
+		return errors.New("PGPDir must not be empty")
+	} else if !strings.HasSuffix(c.Server.PGPDir, "/") {
+		c.Server.PGPDir += "/"
+	}
+
+	if c.Server.PkgDir == "" {
+		return errors.New("PkgDir must not be empty")
+	} else if !strings.HasSuffix(c.Server.PkgDir, "/") {
+		c.Server.PkgDir += "/"
+	}
+
+	if len(c.Server.AllowedHosts) == 0 {
+		return errors.New("AllowedHosts must not be empty")
+	}
+
+	return nil
 }
